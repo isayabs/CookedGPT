9dfc9e9b942c4eab03e5d34ffbc1f3ac0bb46ff2
SoftDevCLF <cintya.lara.f@gmail.com>
2026-03-08 17:28:25 -0600

    Add Styles

---
+++ src/styles/LoginSignUpScreen.styles.js
@@ -0,0 +1,57 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  frame: {
+    flex: 1,
+    backgroundColor: '#F6F0E7',
+    padding: 12,
+  },
+  imageTitle: {
+    width: '100%',
+    height: 100,
+    marginBottom: 16,
+  },
+  imageStyle: {
+    width: '100%',
+    height: 200,
+    marginBottom: 16,
+  },
+  legendText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  forgotPasswordText: {
+    fontSize: 14,
+    color: '#007AFF',
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 12,
+    borderRadius: 4,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  linkText: {
+    color: '#007AFF',
+    marginTop: 16,
+    textAlign: 'center',
+  },
+});
